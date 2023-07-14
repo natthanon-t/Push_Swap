4fdae8c824f3210a63895e867c5113567bf32e73
Natthanon Tairatthanapong <natthanon@McAir.local>
2023-07-14 15:23:17 +0700

    Make it easier

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ntairatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:56:21 by ntairatt          #+#    #+#             */
-/*   Updated: 2023/07/12 23:16:34 by ntairatt         ###   ########.fr       */
+/*   Updated: 2023/07/14 15:18:42 by ntairatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,16 @@ void	push_swap(t_nbr **stack_a, t_nbr **stack_b)
 
 int	main(int ac, char **av)
 {
-	t_nbr	**stack_a;
-	t_nbr	**stack_b;
+	t_nbr	*stack_a;
+	t_nbr	*stack_b;
 
 	if (ac < 2)
 		exit (EXIT_SUCCESS);
-	stack_a = (t_nbr **)malloc(sizeof(t_nbr));
-	if (!stack_a)
-		exit(EXIT_SUCCESS);
-	stack_b = (t_nbr **)malloc(sizeof(t_nbr));
-	if (!stack_b)
-	{
-		ft_clean(stack_a);
-		exit(EXIT_SUCCESS);
-	}
-	*stack_a = NULL;
-	*stack_b = NULL;
-	split_input(stack_a, stack_b, av);
-	sort_index(stack_a);
-	push_swap(stack_a, stack_b);
-	ft_cleanstack(stack_a, stack_b, NULL, 0);
+	stack_a = NULL;
+	stack_b = NULL;
+	split_input(&stack_a, &stack_b, av);
+	sort_index(&stack_a);
+	push_swap(&stack_a, &stack_b);
+	ft_cleanstack(&stack_a, &stack_b, NULL, 0);
 	return (EXIT_SUCCESS);
 }
